1b4668372fe76c379f084aed0f1a770b5c5cfefb
Matt H <lawless-m@users.noreply.github.com>
2018-11-26 16:54:34 +0000

    cached & fxd for sqlitetools

--- EBDB.jl
+++ EBDB.jl
@@ -1,6 +1,9 @@
 module EBDB
 
 using SQLite
+using SQLiteTools
+
+export cleardb, insertEB, allEB
 
 if isfile(raw"c:\users\matt\ntuser.ini")
 	dbdir = raw"C:\Users\matt\Documents\power\SQLite.Data"
@@ -8,7 +11,15 @@ else
 	dbdir = raw"Z:\Maintenance\Matt-H\power\SQLite.Data"
 end
 
-EBdb = SQLite.DB("$dbdir\EB_Perf.db")
+EBdb = SQLite.DB("$dbdir\\EB_Perf.db")
 
+println("$dbdir\\EB_Perf.db")
+
+cleardb() = truncate!(EBdb, "EB")
+
+SQLiteTools.insert!(EBdb, "EBi", "Insert into EB (Date, Leader, Shift, Line, Product, Std_Rate_PPH, Avail_Hours, Product_Max, Product_Avail, Product_Variance, Time_Variance, Efficiency, Item, Process, Problem, Loss_Mins, Effect, OEE_Element, Action, Fix_Or_Repair, Weld_section_due, IncidentNo) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
+
+insertEB(vals) = exebind!("EBi", vals)
+allEB() =  SQLite.query(EBdb, "select * from EB")
 
 end
--- EB_Performance.jl
+++ EB_Performance.jl
@@ -10,19 +10,20 @@ using XlsxWriter
 using ExcelReaders
 using DataArrays
 
-exit()
 
 dir = raw"N:\EB Performance Sheets"
 
 function perfXls(c)
 	for xlfn in sort(filter((f)->f[1] != '~' && f[end-4:end] == ".xlsx" && f[1:4]=="2018", readdir(dir)))
-		Cache.validate_by_mtime!("$dir\\$xlfn", "EBP_$(xlfn)") && put!(c, "$dir\\$xlfn")
+		put!(c, (dir, xlfn))
 	end
 end
 
-function store_sheet(inum, ins, data)
-	SQLite.bind!(ins, 1, Dates.value(data[1,2]))
-	SQLite.bind!(ins, 2, data[1,18])
+function store_sheet(inum, xlfn)
+	data = readxlsheet(xlfn, "EB Line")
+	vals = Vector{Any}(22)
+	vals[1] = Dates.value(data[1,2])
+	vals[2] = data[1,18]
 	
 	for r in 3:50
 		if typeof(data[r,11]) != DataValues.DataValue{Union{}}
@@ -34,59 +35,57 @@ function store_sheet(inum, ins, data)
 		end
 		for k in 1:3
 			if typeof(data[r,k]) != DataValues.DataValue{Union{}} # shift
-				SQLite.bind!(ins, k+2, data[r, k])
+				vals[k+2] = data[r, k]
 			end
 		end
 		if typeof(data[r,4]) != DataValues.DataValue{Union{}} # pph
-			SQLite.bind!(ins, 6, data[r, 4])
+			vals[6] = data[r, 4]
 			for k in 5:12				
 				if typeof(data[r,k]) != DataValues.DataValue{Union{}} # avail hrs
-					SQLite.bind!(ins, k+2, data[r, k])
+					vals[k+2] = data[r, k]
 				end
 			end
-			SQLite.bind!(ins, 8, floor(data[r, 6]))
-			SQLite.bind!(ins, 10, floor(data[r, 8]))
+			vals[8] = floor(data[r, 6])
+			vals[10] = floor(data[r, 8])
 		else
 			if typeof(data[r,3]) != DataValues.DataValue{Union{}}
 				for k = 6:12
-					SQLite.bind!(ins, k, 0)
+					vals[k] = 0
 				end
 			end
 		end
 		for k in 11:19
 			if typeof(data[r,k]) != DataValues.DataValue{Union{}} # Item
-				SQLite.bind!(ins, k+2, data[r, k])
+				vals[k+2] = data[r, k]
 			else
 				if k == 14
-					SQLite.bind!(ins, k+2, 0)
+					vals[k+2] = 0
 				else
-					SQLite.bind!(ins, k+2, "")
+					vals[k+2] = ""
 				end
 			end
 		end
 
 		inum += 1
-		SQLite.bind!(ins, 22, inum)
-		SQLite.execute!(ins)
+		vals[22] = inum
+		insertEB(vals)
 	end
 	inum
 end
 
 function store_sheets()
-	SQLite.query(db, "delete from EB")
-	ins = SQLite.Stmt(db, "Insert into EB (Date, Leader, Shift, Line, Product, Std_Rate_PPH, Avail_Hours, Product_Max, Product_Avail, Product_Variance, Time_Variance, Efficiency, Item, Process, Problem, Loss_Mins, Effect, OEE_Element, Action, Fix_Or_Repair, Weld_section_due, IncidentNo) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
+	cleardb()
 
 	inum = 0
-	for xlfn in Channel(perfXls)
-		println(xlfn)
-		inum = store_sheet(inum, ins, readxlsheet(xlfn, "EB Line"))		
+	for (xld, xlfn) in Channel(perfXls)
+		inum = cache(xlfn, ()->store_sheet(inum, "$xld\\$xlfn"), "EBPerf")
 	end
 end
 
 store_sheets()
 
 function write_sheets(io)
-	rows = SQLite.query(db, "select * from EB")
+	rows = allEB()
 	cells = size(rows[1,1:end],2)-1
 	for row in 1:size(rows,1)
 		print(io, rows[row, 22])
