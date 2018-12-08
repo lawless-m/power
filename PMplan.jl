57a499ae571e49f5f19130beb90e034036f02cd1
Matt H <lawless-m@users.noreply.github.com>
2018-12-08 09:46:12 +0000

    add exporting for import to MS Project

--- PMplan.jl
+++ PMplan.jl
@@ -4,10 +4,9 @@ include("dirs.jl")
 using PMDB
 using XlsxWriter
 
-pms = collect(Channel(pm_list))
 lns = lines()
 
-function gather_events()
+function gather_events(pms)
 	s = Date(now())
 	e = s + Dates.Day(30)
 	events = Dict{Date, Dict{String, Vector{Int}}}()
@@ -40,8 +39,11 @@ function max_items_per_line(events)
 	maxs
 end
 
-function write_events(events)
-	wb = Workbook("$home\\power\\PMs\\PM_Plan2.xlsx")
+function write_events()
+	pms = collect(Channel(pm_list))
+	events = gather_events(pms)
+	
+	wb = Workbook("$home\\power\\PMs\\PM_Plan.xlsx")
 	ws = add_worksheet!(wb, "Plan")
 	wraptop = add_format!(wb, Dict("text_wrap"=>true, "valign"=>"top"))
 
@@ -76,9 +78,12 @@ function write_events(events)
 			r += maxs[l]
 		end
 		range = rc2cell(1, c) * ":" * rc2cell(r-1, c)
-		write_formula!(ws, r, c, "=concatenate(\"Scheduled Hrs: \", sum($range))")
-		write_formula!(ws, r+1, c, "=concatenate(\"Unknown time: \", COUNTIF($range,\"=0\"))")
-		write_formula!(ws, r+2, c, "=concatenate(\"No. Tasks: \", COUNTIF($range,\">=0\"))")
+		write!(ws, r, c, "Scheduled Hrs:")
+		write_formula!(ws, r, c+1, "=sum($range)")
+		write!(ws, r+1, c, "Unknown time:")
+		write_formula!(ws, r+1, c+1, "=COUNTIF($range,\"=0\")")
+		write!(ws, r+2, c, "No. Tasks:")
+		write_formula!(ws, r+2, c+1, "=COUNTIF($range,\">=0\")")
 		
 		c += 2
 	end
@@ -87,5 +92,26 @@ function write_events(events)
 	
 end
 
-write_events(gather_events())
+#write_events()
+
+function write_for_project()
+	wb = Workbook("$home\\power\\PMs\\PM_Proj.xlsx")
+	ws = add_worksheet!(wb, "Project")
+	date_format = add_format!(wb, Dict("num_format"=>"dd/mm/yyyy hh:mm"))
+	
+	day(x) = x>0? x/24 : 0
+	eod(d) = DateTime(d) + Dates.Minute(1439)
+	
+	write_row!(ws, 0, 0, ["Deadline", "Resource Names", "Duration", "Name", "Notes"])
+	r = 1
+	for pm in Channel(pm_list)
+		write_datetime!(ws, r, 0, eod(pm[:DueDate]), date_format)
+		write_row!(ws, r, 1, [pm[:ID], day(pm[:ScheduledHours]), pm[:Title], pm[:Priority]])
+		r += 1
+	end
+	close(wb)
+end
+
+write_for_project()
+
 
