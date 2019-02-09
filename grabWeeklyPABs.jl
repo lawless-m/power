9a918ff1654c60f28f89c4b9cb219286d370c3c3
Matt H <lawless-m@users.noreply.github.com>
2019-02-09 11:53:43 +0000

    fold in function

--- grabWeeklyPABs.jl
+++ grabWeeklyPABs.jl
@@ -1,7 +1,17 @@
 include("dirs.jl")
 
-using PAB
-
+function readweeks(io, line, dir)
+    println(dir)
+    for wk in filter((f)->f[1] == 'W' && f[end-3:end] == ".xls", readdir(dir))
+        wknum = wk[6:end-4]
+        sht = sheet(dir, wk, "Quality")
+        c = 3
+        while typeof(sht[2,c]) == String
+            println(io, wknum, "\t", sht[2,c], "\t", sht[3,c])
+            c += 1
+        end
+    end
+end
 
 function homes()
 
