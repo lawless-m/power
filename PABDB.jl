a6abd6aa28bdae15305accfe0d1e23544f8c2e55
Matt H <lawless-m@users.noreply.github.com>
2019-02-20 13:56:30 +0000

    add new functions

--- PABDB.jl
+++ PABDB.jl
@@ -13,12 +13,10 @@ SQLiteTools.insert!(PabDB, "PABi", "INSERT INTO PAB (Line, StartT, EndT, Reason,
 SQLiteTools.insert!(PabDB, "Faulti", "INSERT INTO Faults (Line, Stage, Fault) VALUES(?,?,?)")
 SQLiteTools.insert!(PabDB, "AvailLossi", "INSERT INTO AvailabilityLoss (PAB_ID, Fault_ID, Loss) VALUES(?,?,?)")
 
-
 function insertPAB!(vals)
     if size(SQLite.query(PabDB, "SELECT Line FROM PAB WHERE Line=? AND StartT=? AND EndT=?", values=[vals[1], vals[2], vals[3]]), 1) > 0
         return 0
     end
-
     exebind!("PABi", vals)
     return last_insert(PabDB)
 end
@@ -34,5 +32,25 @@ function faultID(line, stage, fault)
     exebind!("Faulti", [line, stage, fault])
     SQLite.query(PabDB, "SELECT last_insert_rowId()")[1][1]
 end
+
+function faultList()
+    faults = Dict{String, Dict{String, Dict{String, Int}}}()
+    flts = SQLite.query(PabDB, "SELECT * FROM Faults")
+    for r in 1:size(flts, 1)
+        ld = get!(faults, flts[r, :Line], Dict{String, Dict{String, Int}}())
+        sd = get!(ld, flts[r, :Stage], Dict{String, Int}())
+        sd[flts[r, :Fault]] = flts[r, :Fault_ID]
+    end
+    faults
+end
+
+function pabsBetween(s::DateTime, e::DateTime)
+    SQLite.query(PabDB, "SELECT * FROM PAB WHERE StartT >=? AND EndT <=? order by Line, StartT desc", values=[Dates.value(s), Dates.value(e)], stricttypes=true)
+end
+
+function availLossBetween(s::DateTime, e::DateTime)
+    SQLite.query(PabDB, "SELECT AvailabilityLoss.* FROM PAB JOIN AvailabilityLoss on PAB.id=AvailabilityLoss.PAB_ID  WHERE StartT >=? AND EndT <=?", values=[Dates.value(s), Dates.value(e)])
+end
+
 ###############
 end
