0f7ccbe521bfb2184059e45f6af4b77abf25d4c5
Matt H <lawless-m@users.noreply.github.com>
2019-05-26 10:58:49 +0100

    joinpath instead

--- dirs.jl
+++ dirs.jl
@@ -1,18 +1,19 @@
 
 
+Home = ""
+
 if isfile(raw"c:\users\matt\ntuser.ini")
-	home = raw"C:\Users\matt\Documents\power"
+	Home = raw"C:\Users\matt\Documents\power"
 elseif isfile("/home/matt/.huge")
-	home = "/home/matt/"
+	Home = "/home/matt/"
 else
-	home = raw"Z:\Maintenance\Matt-H"
+	Home = raw"Z:\Maintenance\Matt-H"
 end
 
-if ! (joinpath(home, "GitHub", "power" in LOAD_PATH)
-	for lib in readdir(joinpath(home, "GitHub"))
-		push!(LOAD_PATH, joinpath(home, GitHub, lib))
+if ! (joinpath(Home, "GitHub", "power") in LOAD_PATH)
+	for lib in readdir(joinpath(Home, "GitHub"))
+		push!(LOAD_PATH, joinpath(Home, "GitHub", lib))
 	end
 end
 
-dbdir = joinpath(home, "SQLite.Data")
-
+DBDir = joinpath(Home, "SQLite.Data")
